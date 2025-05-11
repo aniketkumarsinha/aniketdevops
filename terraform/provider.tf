9585b5afd253cad8cfae3bc34890ab2e8f00af8e
aniket-sinha8 <sinhaaniket32@gmail.com>
2025-05-11 11:36:50 +0530

    updated helm provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -25,45 +25,56 @@ provider "azurerm" {
 }
 
 # Helm provider for AKS ArgoCD cluster (uses exec auth)
+# provider "helm" {
+#   alias = "argocd"
+#   kubernetes {
+#     host                   = azurerm_kubernetes_cluster.aks_argocd.kube_config[0].host
+#     cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks_argocd.kube_config[0].cluster_ca_certificate)
+
+#     exec {
+#       api_version = "client.authentication.k8s.io/v1beta1"
+#       command     = "az"
+#       args        = [
+#         "aks",
+#         "get-credentials",
+#         "--resource-group", azurerm_kubernetes_cluster.aks_argocd.resource_group_name,
+#         "--name", azurerm_kubernetes_cluster.aks_argocd.name,
+#         "--admin",
+#         "--file", "/dev/stdout"
+#       ]
+#     }
+#   }
+# }
+
 provider "helm" {
   alias = "argocd"
+  debug = true
   kubernetes {
-    host                   = azurerm_kubernetes_cluster.aks_argocd.kube_config[0].host
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks_argocd.kube_config[0].cluster_ca_certificate)
-
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "az"
-      args        = [
-        "aks",
-        "get-credentials",
-        "--resource-group", azurerm_kubernetes_cluster.aks_argocd.resource_group_name,
-        "--name", azurerm_kubernetes_cluster.aks_argocd.name,
-        "--admin",
-        "--file", "/dev/stdout"
-      ]
-    }
+    host                   = data.azurerm_kubernetes_cluster.aks_argocd.kube_admin_config[0].host
+    client_key             = base64decode(data.azurerm_kubernetes_cluster.aks_argocd.kube_admin_config[0].client_key)
+    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.aks_argocd.kube_admin_config[0].client_certificate)
+    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks_argocd.kube_admin_config[0].cluster_ca_certificate)
   }
 }
 
 # Helm provider for AKS WebApp cluster (uses exec auth)
-provider "helm" {
-  alias = "webapp"
-  kubernetes {
-    host                   = azurerm_kubernetes_cluster.aks_webapp.kube_config[0].host
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks_webapp.kube_config[0].cluster_ca_certificate)
+# provider "helm" {
+#   alias = "webapp"
+#   kubernetes {
+#     host                   = azurerm_kubernetes_cluster.aks_webapp.kube_config[0].host
+#     cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks_webapp.kube_config[0].cluster_ca_certificate)
 
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "az"
-      args        = [
-        "aks",
-        "get-credentials",
-        "--resource-group", azurerm_kubernetes_cluster.aks_webapp.resource_group_name,
-        "--name", azurerm_kubernetes_cluster.aks_webapp.name,
-        "--admin",
-        "--file", "/dev/stdout"
-      ]
-    }
-  }
-}
+#     exec {
+#       api_version = "client.authentication.k8s.io/v1beta1"
+#       command     = "az"
+#       args        = [
+#         "aks",
+#         "get-credentials",
+#         "--resource-group", azurerm_kubernetes_cluster.aks_webapp.resource_group_name,
+#         "--name", azurerm_kubernetes_cluster.aks_webapp.name,
+#         "--admin",
+#         "--file", "/dev/stdout"
+#       ]
+#     }
+#   }
+# }
